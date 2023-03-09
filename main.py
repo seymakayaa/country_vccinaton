37d759a0cb26061101aa4cf221aadd101deccf54
Seyma Kaya <seymakaya@Seyma-MacBook-Air.local>
2023-03-09 15:40:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('country_vaccination_stats.csv')
+
+print(dataset.isnull().sum())
+
+#To find null values in dataset we use
+print(dataset.isnull().sum())
+
+# Assign to null values what value
+dataset.daily_vaccinations.fillna(value= "0")
+
